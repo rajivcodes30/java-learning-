3fe959ac7562b23ef374fb65327edfe7f6bccd8f
rajivcodes30 <tiwarirajiv0910@gmail.com>
2026-07-28 16:20:34 +0530

    Day 6: Methods in Java

---
+++ Methods.java
@@ -0,0 +1,26 @@
+public class Methods  {
+    static int add(int a, int b) {
+        return a+b;
+
+    }
+     static int multiply (int a, int b) {
+        return a*b;
+
+     }
+      static int divide (int a, int b){
+        return a/b;
+
+      }
+      public static void main (String[] args){
+        int a =20;
+        int b =10;
+        System.out.println("Addition = " +add(a,b));
+        System.out.println("Multiplication = " +multiply(a,b));
+        System.out.println("Division ="+ divide(a,b));
+
+
+      }
+
+    }
+    
+
