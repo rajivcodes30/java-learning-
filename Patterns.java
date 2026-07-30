7db64ba4125a3c81416b30a0fea1c06e4240b6fc
rajivcodes30 <tiwarirajiv0910@gmail.com>
2026-07-30 08:05:56 +0530

    Day 8: Strings in Java

--- Patterns.java
+++ Patterns.java
@@ -14,13 +14,13 @@ public class Patterns {
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
-            }
+          }
             System.out.println();
         }
 
         System.out.println();
 
-        
+
         for (int i = 1; i <= rows; i++) {
             for (int j = rows; j > i; j--) {
                 System.out.print(" ");
