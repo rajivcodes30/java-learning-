78c92513941359b2b75ad36773a451c8ab3c2900
rajivcodes30 <tiwarirajiv0910@gmail.com>
2026-08-03 15:16:25 +0530

    Day 12: Fibonacci Numbers in Java

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        long first = 0, second = 1;
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            long next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+
+}
