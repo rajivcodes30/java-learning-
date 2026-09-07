28353398faee96aed16214a5dd55db91ec883ad6
rajivcodes30 <tiwarirajiv0910@gmail.com>
2026-09-07 21:14:59 +0530

    Day 46: Level Order Traversal in  Java

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.remove();
+
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.print("Level Order: ");
+        levelOrder(root);
+    }
+}
