2560abe744d3382458056fe0306618a3061f609f
rajivcodes30 <tiwarirajiv0910@gmail.com>
2026-09-22 22:02:12 +0530

    Day 56 : Right View Of Binary Tree

---
+++ RightViewBinaryTree.java
@@ -0,0 +1,43 @@
+public class RightViewBinaryTree {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void rightView(Node root, int level, int[] maxLevel) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (level > maxLevel[0]) {
+            System.out.print(root.data + " ");
+            maxLevel[0] = level;
+        }
+
+        rightView(root.right, level + 1, maxLevel);
+        rightView(root.left, level + 1, maxLevel);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        root.right.right = new Node(60);
+
+        int[] maxLevel = {-1};
+
+        System.out.print("Right View: ");
+        rightView(root, 0, maxLevel);
+    }
+}
